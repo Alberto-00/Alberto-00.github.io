30a3360575fb1b83b5ccfd2926faac5009d6b62b
Alberto-00 <albmont99@gmail.com>
2022-02-21 00:31:49 +0100

    page
    insert contact and resume

---
+++ src/main/webapp/js/text-change.js
@@ -0,0 +1,28 @@
+$(document).ready(function (){
+    $('.txt-eff1').each(function(){
+        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+    });
+
+    anime.timeline({loop: true})
+        .add({
+            targets: '.txt-eff1 .letter',
+            translateX: [40,0],
+            translateZ: 0,
+            opacity: [0,1],
+            easing: "easeOutExpo",
+            duration: 1200,
+            delay: function(el, i) {
+                return 500 + 30 * i;
+            }
+        }).add({
+        targets: '.txt-eff1 .letter',
+        translateX: [0,-30],
+        opacity: [1,0],
+        easing: "easeInExpo",
+        duration: 1100,
+        delay: function(el, i) {
+            return 100 + 30 * i;
+        }
+    });
+
+})
