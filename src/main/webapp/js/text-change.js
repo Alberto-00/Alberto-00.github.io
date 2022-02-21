847a273d22617f88f83f55dbf67c3463beb1e6bc
Alberto-00 <albmont99@gmail.com>
2022-02-21 22:15:31 +0100

    update code

--- src/main/webapp/js/text-change.js
+++ src/main/webapp/js/text-change.js
@@ -1,28 +1,45 @@
 $(document).ready(function (){
-    $('.txt-eff1').each(function(){
-        $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
-    });
 
-    anime.timeline({loop: true})
-        .add({
+    var text = ["CyberSecurity Addicted", "AI Addicted", "Computer Science Student"];
+    var counter = 0;
+    firstChange($('.txt-eff1'));
+    var inst = setInterval(change, 3950);
+
+    function firstChange(elem){
+        elem.each(function(){
+            $(this).html($(this).text().replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>"));
+        });
+
+        anime.timeline({loop: true})
+            .add({
+                targets: '.txt-eff1 .letter',
+                translateX: [40,0],
+                translateZ: 0,
+                opacity: [0,1],
+                easing: "easeOutExpo",
+                duration: 1200,
+                delay: function(el, i) {
+                    return 500 + 30 * i;
+                }
+            }).add({
             targets: '.txt-eff1 .letter',
-            translateX: [40,0],
-            translateZ: 0,
-            opacity: [0,1],
-            easing: "easeOutExpo",
-            duration: 1200,
+            translateX: [0,-30],
+            opacity: [1,0],
+            easing: "easeInExpo",
+            duration: 1100,
             delay: function(el, i) {
-                return 500 + 30 * i;
+                return 100 + 30 * i;
             }
-        }).add({
-        targets: '.txt-eff1 .letter',
-        translateX: [0,-30],
-        opacity: [1,0],
-        easing: "easeInExpo",
-        duration: 1100,
-        delay: function(el, i) {
-            return 100 + 30 * i;
-        }
-    });
+        });
+    }
 
+    function change() {
+        const elem = $('.txt-eff1');
+        elem.text(text[counter]);
+        counter++;
+        if (counter >= text.length) {
+            counter = 0;
+        }
+        firstChange(elem);
+    }
 })
